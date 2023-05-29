0cccd3284328bab05d8075c6397a0b9c4859d1c1
Mariana Ramiro <marianaramiroof@gmail.com>
2023-05-29 20:18:59 -0300

    'Requisito 2'

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,14 +1,19 @@
 const TOKEN = import.meta.env.VITE_TOKEN;
-const fetchWeatherByTerm = (term) => fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`);
+const fetchSearchByTerm = (term) => fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`);
 
 export const searchCities = async (term) => {
-  const response = await fetchWeatherByTerm(term);
+  const response = await fetchSearchByTerm(term);
   const data = await response.json();
   const { name } = data;
   if (name !== term) window.alert('Nenhuma cidade encontrada');
   return data;
 };
 
-export const getWeatherByCity = (/* cityURL */) => {
-//   seu código aqui
+const fetchWeatherByCityUrl = (cityURL) => `http://api.weatherapi.com/v1/current.json?lang=pt&key=${TOKEN}&q=${cityURL}`;
+
+export const getWeatherByCity = async (cityURL) => {
+  const response = await fetchWeatherByCityUrl(cityURL);
+  const data = await response.json();
+  const { temp_c, condition.text, condition.icon } = data.current;
+  return {`temp: ${temp_c}, condition: ${condition.text}, icon: ${condition.icon}`};
 };
