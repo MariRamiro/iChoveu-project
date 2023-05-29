913fe704836c0ddedeccd8d47176032cf8f272dd
Mariana Ramiro <marianaramiroof@gmail.com>
2023-05-29 19:49:20 -0300

    'Requisito 1'

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,12 @@
-// Remova os comentários a medida que for implementando as funções
+const TOKEN = import.meta.env.VITE_TOKEN;
+const fetchWeatherByTerm = (term) => fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`);
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  const response = await fetchWeatherByTerm(term);
+  const data = await response.json();
+  const { name } = data;
+  if (name !== term) window.alert('Nenhuma cidade encontrada');
+  return data;
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
